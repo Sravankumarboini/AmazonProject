816605e44aa58a38d9cb70e69568083973b1abfa
Sravankumarboini <sravankumarboini005@gmail.com>
2025-10-13 20:51:41 +0530

    add javascript for index.html

---
+++ Scripts/amazon.js
@@ -0,0 +1,25 @@
+const products =[{
+    Image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating:{
+        stars:4.5,
+        count:87
+    },
+    price:1090
+},{
+    Image:'images/products/intermediate-composite-basketball.jpg',
+    name:' Intermediate Size Basketball',
+    rating:{
+        stars:4.5,
+        count:127
+    },
+    price:2095
+},{
+    Image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:' Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        stars:4.5,
+        count:55
+    },
+    price:799
+}];
